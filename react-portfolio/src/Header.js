d019b8d4ab4f96d27fc6df6aeccde6bcf2c75125
whplug <whplug@gmail.com>
2023-09-28 19:54:59 +0200

    First set-up of portfolio site

---
+++ react-portfolio/src/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <section class="header">
+        <h1>Working Title</h1>
+        <img src="./favicon.png" alt="This is me in pixel art"></img>
+    </section>
+  );
+}
+
+export default Header;
